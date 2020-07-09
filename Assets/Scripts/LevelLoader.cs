2822c90adf53bcb8635810e678b1bfc535eb2eb0
Mike Davis <mikedavisrocha@gmail.com>
2020-07-09 09:32:48 -0300

    First commit

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] float delay = 1f;
+
+    public void LoadGameOver()
+    {
+        StartCoroutine(LoadDelayedGameOver());
+    }
+
+    IEnumerator LoadDelayedGameOver()
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene("GameOver");
+    }
+
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene("Game");
+        FindObjectOfType<GameSession>().ResetGame();
+    }
+
+    public void LoadStartMenu()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+
+    public void QuitMenu()
+    {
+        Application.Quit();
+    }
+}
